82edf55d04d25f1a0da93e94df614f814295c4ec
romainberthome <romainberthome@gmail.com>
2017-07-20 17:15:51 +0000

    socket.io integration

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 var path = require("path");
 var publicPath = path.join(__dirname, "../public");
 var express = require("express");
+var http = require("http");
+var socketIO = require("socket.io");
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+io.on('connection', function (socket) {
+  console.log("New user connected");
+  socket.on('disconnect', function () {
+    console.log("User is disconnected");
+  });
+});
+
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, process.env.IP, function(){
+server.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started");
 });
 
